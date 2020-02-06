f85ca5b55f9af8935ee3763330a2f475140fc511
Ted M Lin <tedmlin@gmail.com>
2020-02-06 14:16:38 -0500

    Reduced feature config

--- keyboards/kyria/keymaps/kitlaan/config.h
+++ keyboards/kyria/keymaps/kitlaan/config.h
@@ -21,7 +21,19 @@
 #endif
 
 #ifdef RGBLIGHT_ENABLE
+  // turn on all animations, or just the ones you want (to reduce flash usage)
   #define RGBLIGHT_ANIMATIONS
+  // See: quantum/rgblight_reconfig.h
+  //#define RGBLIGHT_EFFECT_ALTERNATING
+  //#define RGBLIGHT_EFFECT_BREATHING
+  //#define RGBLIGHT_EFFECT_CHRISTMAS
+  //#define RGBLIGHT_EFFECT_KNIGHT
+  //#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  //#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+  //#define RGBLIGHT_EFFECT_RGB_TEST
+  //#define RGBLIGHT_EFFECT_SNAKE
+  //#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
@@ -30,3 +42,10 @@
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 // #define SPLIT_USB_DETECT
 // #define NO_USB_STARTUP_CHECK
+
+// Disable. We're not using KC_LCAP, KC_LNUM or KC_LSCR.
+#undef LOCKING_SUPPORT_ENABLE
+
+// Disable. Not currently using these features
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_TAPPING
--- keyboards/kyria/keymaps/kitlaan/rules.mk
+++ keyboards/kyria/keymaps/kitlaan/rules.mk
@@ -1,3 +1,16 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+LEADER_ENABLE = yes        # Leader-key
+MOUSEKEY_ENABLE = no       # Don't need mouse buttons
+
+# TODO: research which works better for me
+# https://www.reddit.com/r/olkb/comments/7ug57n/qmk_space_cadet_vs_mod_tap/
+SPACE_CADET_ENABLE = no
+
+# Disable runtime change of EEPROM keymap config (process_magic.c)
+MAGIC_ENABLE = no
+
+# Use to reset EEPROM if COL0+ROW0 held down at boot
+#BOOTMAGIC_ENABLE = lite
